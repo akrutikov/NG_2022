30e75e358eed1957c9fdba5951a34434b9bbca9c
Alex <sanakrutikov9@gmail.com>
2022-04-17 20:00:01 +0300

    the program displays a picture

---
+++ Lesson 1/Task 2/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << ",         ," << endl
+         << "|\\\\\\\\ ////|" << endl
+         << "| \\\\\\V/// |" << endl
+         << "|  |~~~|  |" << endl
+         << "|  |===|  | " << endl
+         << "|  |j  |  |" << endl
+         << "|  | g |  |" << endl
+         << " \\ |  s| /" << endl
+         << "  \\|===|/" << endl
+         << "   '---'";
+}
