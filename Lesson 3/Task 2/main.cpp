6886c94e768f836565780d8856936281d1e26b31
Alex <sanakrutikov9@gmail.com>
2022-05-28 17:19:34 +0300

    fill the array with random variables and simplified the code

--- Lesson 3/Task 2/main.cpp
+++ Lesson 3/Task 2/main.cpp
@@ -1,19 +1,25 @@
 #include <iostream>
+#include <time.h>
 
 using namespace std;
 
 int main()
 {
     int arr1[4], arr2[4], sum1 = 0, sum2 = 0;
-
+    cout << "Your first array is: ";
     for(int i = 0; i < 4; i++){
-        cout << "Your first array is: ";
-        cin >> arr1[i];
+        arr1[i] = rand() % 9;
+        cout << arr1[i];
+        cout << " ";
     }
+    cout << endl;
+    cout << "Your second array is: ";
     for(int j = 0; j < 4; j++){
-        cout << "Your second array is: ";
-        cin >> arr2[j];
+        arr2[j] = rand() % 9;
+        cout << arr2[j];
+        cout << " ";
     }
+    cout << endl;
     for(int i = 0; i < 4; i++){
         sum1 += arr1[i];
         if(i < 3)
