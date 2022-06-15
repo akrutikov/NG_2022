f3787704749ac3f59bdbb14231aedc3e64cc67cb
Alex <sanakrutikov9@gmail.com>
2022-06-15 19:57:42 +0300

    add Task 5.4

---
+++ Lesson 5/Task 4/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int Discriminat()
+{
+    double a, b, c, D, x1, x2;
+    cout << "Enter value a: ";
+    cin >> a;
+    cout << "Enter value b: ";
+    cin >> b;
+    cout << "Enter value c: ";
+    cin >> c;
+    cout << "D = " << D<< endl;
+    if (D > 0){
+        x1 = ((-b) + sqrt(D)) / (2 * a);
+        x2 = ((-b) - sqrt(D)) / (2 * a);
+        cout << "x1 = " << x1 << endl;
+        cout << "X2 = " << x2;
+    } else if (D == 0){
+        x1 = -b / 2 * a;
+        cout << "x = " << x1;
+    } else if (D < 0){
+        cout << "Discriminat < 0 no answer";
+    }
+    return 0;
+}
+
+
+int main()
+{
+    cout << Discriminat();
+}
